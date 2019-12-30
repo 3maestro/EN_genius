e76ceae61a4ad19d5a405c8a5458bac759fb4d18
Administrator <Administrator@MSDN-SPECIAL>
2019-12-30 17:37:00 +0900

    [정상혁] 작업 공임 작업중
    2019-12-30 17:36

--- EN_genius/src/main/java/com/cafe24/radev/controller/WageController.java
+++ EN_genius/src/main/java/com/cafe24/radev/controller/WageController.java
@@ -29,14 +29,15 @@ public class WageController {
 		System.out.println(ccCode + " <-ccCode 차량 조회 성공시 세션에 등록된 배기량 기준 코드");
 		int num = 1;
 		List<WageManHour> list = wageService.getWorkManHour(ccCode);
-		for(int i=0; i<list.size(); i++) {
-			if(i > 0) {
-				num = i;
-				model.addAttribute("num",num);
-			}
-		}
+//		for(int i=0; i<list.size(); i++) {
+//			if(i > 0) {
+//				num = i;
+//				model.addAttribute("num",num);
+//			}
+//		}
 		//System.out.println(list + " <-list workManHour WageController.java");
 		model.addAttribute("wageManHour",list);
+		model.addAttribute("num",list.size());
 		
 		return "/wage/workManHour";
 	}
