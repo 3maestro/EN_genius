c145994016dfd14a71b37e3c48a8e02d9d6134a0
Administrator <Administrator@MSDN-SPECIAL>
2019-12-30 14:12:23 +0900

    [정상혁]

--- EN_genius/src/main/java/com/cafe24/radev/controller/WageController.java
+++ EN_genius/src/main/java/com/cafe24/radev/controller/WageController.java
@@ -27,10 +27,17 @@ public class WageController {
 		System.out.println("workManHour WageController 호출");
 		String ccCode = (String)session.getAttribute("CCCODE");
 		System.out.println(ccCode + " <-ccCode 차량 조회 성공시 세션에 등록된 배기량 기준 코드");
-		
+		int num = 1;
 		List<WageManHour> list = wageService.getWorkManHour(ccCode);
+		for(int i=0; i<list.size(); i++) {
+			if(i > 0) {
+				num = i;
+				model.addAttribute("num",num);
+			}
+		}
 		//System.out.println(list + " <-list workManHour WageController.java");
 		model.addAttribute("wageManHour",list);
+		
 		return "/wage/workManHour";
 	}
 	
