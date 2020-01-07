210216d30b236b7284f4f82d283ea4bd22ef309f
parkyeonwoo <Administrator@smart20113>
2020-01-07 11:56:37 +0900

    [박연우] 2020-01-07 11:56

--- EN_genius/src/main/java/com/cafe24/radev/vo/CarFactory.java
+++ EN_genius/src/main/java/com/cafe24/radev/vo/CarFactory.java
@@ -23,6 +23,7 @@ public class CarFactory {
 	private String bsTelephone;
 	private String bsRegistration;
 	private String bsWriter;
+	private String bsDate;
 	private String bsCheck;
 	public String getBsCode() {
 		return bsCode;
@@ -120,6 +121,12 @@ public class CarFactory {
 	public void setBsWriter(String bsWriter) {
 		this.bsWriter = bsWriter;
 	}
+	public String getBsDate() {
+		return bsDate;
+	}
+	public void setBsDate(String bsDate) {
+		this.bsDate = bsDate;
+	}
 	public String getBsCheck() {
 		return bsCheck;
 	}
@@ -132,7 +139,8 @@ public class CarFactory {
 				+ ", bossName=" + bossName + ", bossPhonenum=" + bossPhonenum + ", bossEmail=" + bossEmail
 				+ ", emailAddr=" + emailAddr + ", bsName=" + bsName + ", bsNumber=" + bsNumber + ", bsFaxNumber="
 				+ bsFaxNumber + ", bsAddr=" + bsAddr + ", addrDetail=" + addrDetail + ", bsTelephone=" + bsTelephone
-				+ ", bsRegistration=" + bsRegistration + ", bsWriter=" + bsWriter + ", bsCheck=" + bsCheck + "]";
+				+ ", bsRegistration=" + bsRegistration + ", bsWriter=" + bsWriter + ", bsDate=" + bsDate + ", bsCheck="
+				+ bsCheck + "]";
 	}
-	
+
 }
